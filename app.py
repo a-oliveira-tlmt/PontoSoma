28eb5c0d0f9b951bedbcb10d7d0e791f6fb6e4f6
a-oliveira-tlmt <32546796+a-oliveira-tlmt@users.noreply.github.com>
2017-10-20 22:41:09 -0300

    Atualizar para definição padrão da disciplina
    if __name__ ...

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from modules.ponto import Point
 
-P1 = Point(2,5)
-P2 = Point(3,4)
+def main(args=[]):
+    P1 = Point(2,5)
+    P2 = Point(3,4)
+    PS = P1 + P2
 
-PS = P1 + P2
+    print("Ponto Soma definido:")
 
-print("Ponto Soma definido:")
+    print(P1)
+    
+    print('        +')
+    
+    print(P2)
+    
+    print('        =')
 
-print(P1)
+    print(PS)
 
-print('        +')
-
-print(P2)
-
-print('        =')
-
-print(PS)
+if __name__ == "__main__":
+    main()
+    
