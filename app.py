00c4c4ed79c8544bcfcdcfd9c0fe7088aa35e227
a-oliveira-tlmt <32546796+a-oliveira-tlmt@users.noreply.github.com>
2017-10-26 12:08:20 -0300

    Atualização do Código app.py
    Inserção de:
    - Solicitação de dados do usuário
    - Teste de igualdade entre P1 e P2

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from modules.ponto import Point
 
 def main(args=[]):
-    P1 = Point(2,5)
-    P2 = Point(3,4)
+    inputx = int(input("\nInsira o valor de X1 >> "))
+    inputy = int(input("\nInsira o valor de Y1 >> "))
+
+    P1 = Point(inputx, inputy)
+
+    inputx = int(input("\nInsira o valor de X2 >> "))
+    inputy = int(input("\nInsira o valor de Y2 >> "))
+
+    P2 = Point(inputx, inputy)
+
     PS = P1 + P2
 
-    print("Ponto Soma definido:")
+    print("\nPonto Soma definido:\n")
 
     print(P1)
     
@@ -16,6 +24,12 @@ def main(args=[]):
     print('        =')
 
     print(PS)
+
+    print("\nTeste de Igualdade entre P1 e P2: ")
+    print(P1 == P2)
+    
+    print(' ')
+
 
 if __name__ == "__main__":
     main()
